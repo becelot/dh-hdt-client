3e7bebc1591640c2d8f3b48b6ec433880d5a947a
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-17 12:46:28 +0200

    Added login logic to options flyout

--- DeckHistoryPlugin/FlyoutControls/OptionsFlyout.xaml.cs
+++ DeckHistoryPlugin/FlyoutControls/OptionsFlyout.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using DeckHistoryPlugin.Api;
+using Hearthstone_Deck_Tracker.Annotations;
+using Hearthstone_Deck_Tracker.Utility;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +23,110 @@ namespace DeckHistoryPlugin.FlyoutControls
     /// <summary>
     /// Interaction logic for OptionsFlyout.xaml
     /// </summary>
-    public partial class OptionsFlyout : UserControl
+    public partial class OptionsFlyout : UserControl, INotifyPropertyChanged
     {
+        private bool _showAuthenticationSpinner = false;
+        private bool _showLoginHint = false;
+        private string _loginHint = "Some login hint";
+
         public OptionsFlyout()
         {
             InitializeComponent();
+            ApiWrapper.LoggedIn += () => UpdateAccount();
+            ApiWrapper.LoggedOut += () => UpdateAccount();
+        }
+
+        private void UpdateAccount()
+        {
+            OnPropertyChanged(nameof(IsAuthenticated));
+            OnPropertyChanged(nameof(LoggedInUser));
+
+            ShowLoginHint = false;
+        }
+
+        #region ViewModel
+        public bool IsAuthenticated => Account.Instance.IsAuthenticated;
+
+        public string LoggedInUser => Account.Instance.Username;
+
+        public bool ShowLoginHint
+        {
+            get
+            {
+                return _showLoginHint;
+            }
+            set
+            {
+                if (_showLoginHint == value) return;
+                _showLoginHint = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsAuthenticating
+        {
+            get
+            {
+                return _showAuthenticationSpinner;
+            }
+            set
+            {
+                if (_showAuthenticationSpinner == value) return;
+                _showAuthenticationSpinner = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public string LoginHint
+        {
+            get
+            {
+                return _loginHint;
+            }
+            set
+            {
+                if (_loginHint == value) return;
+                _loginHint = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Events
+        public ICommand LoginCommand => new Command(async () =>
+        {
+            var username = Username.Text;
+
+            if (String.IsNullOrEmpty(username))
+            {
+                return;
+            }
+
+            var password = Password.Password;
+
+            if (String.IsNullOrEmpty(password))
+            {
+                return;
+            }
+
+            IsAuthenticating = true;
+            var result = await ApiWrapper.Login(username, password);
+            IsAuthenticating = false;
+            LoginHint = result;
+            ShowLoginHint = true;
+        });
+
+        public ICommand LogoutCommand => new Command(() => ApiWrapper.Logout());
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
