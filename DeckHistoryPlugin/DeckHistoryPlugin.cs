74f24a9228005de3e0c01aacf3f81c842a164720
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-05-17 10:36:38 +0200

    Added dependencies on Hearthstone-Deck-Tracker

---
+++ DeckHistoryPlugin/DeckHistoryPlugin.cs
@@ -0,0 +1,45 @@
+﻿using Hearthstone_Deck_Tracker.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DeckHistoryPlugin
+{
+    public class DeckHistoryPlugin : IPlugin
+    {
+        public string Name => "Deck History";
+
+        public string Description => "A plugin that tracks and uploads your played decks history. Connects to the Twitch extension to display recently played decks to your viewer!";
+
+        public string ButtonText => "Configure";
+
+        public string Author => "Becelot";
+
+        public Version Version => new Version(0, 0, 1);
+
+        public MenuItem MenuItem => null;
+
+
+        public void OnButtonPress()
+        {
+
+        }
+
+        public void OnLoad()
+        {
+
+        }
+
+        public void OnUnload()
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+        }
+    }
+}
